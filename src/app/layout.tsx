2a438beee3349144fad138a6114f3e2520e66593
Huu Phuong <phuonghuu71@gmail.com>
2023-10-19 13:07:42 +0700

    Change Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Welcome to My Portfolio",
   description: "Welcome to my Portfolio",
 };
 
